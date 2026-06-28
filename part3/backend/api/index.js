c227e8cd1192ebc13ae308967c3b4668fccbcda1
BagasApg <bagasapg@gmail.com>
2026-06-28 21:54:36 +0700

    part3 - fixed api/ dir

--- part3/backend/api/index.js
+++ part3/backend/api/index.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, '../dist')))
 
 let notes = [
    { id: "1", content: "HTML is easy", important: true },
@@ -42,9 +42,9 @@ app.post('/api/notes', (request, response) => {
 
 })
 
-app.get('/', (request, response) => {
-   response.send("<h1>Hello World!</h1>")
-})
+// app.get('/', (request, response) => {
+//    response.send("<h1>Hello World!</h1>")
+// })
 
 app.get('/api/notes', (request, response) => {
    response.json(notes)
@@ -69,7 +69,12 @@ app.delete('/api/notes/:id', (request, response) => {
    response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-   console.log(`Server running on port ${PORT}`)
-})
+if (process.env.NODE_ENV !== 'production') {
+   const PORT = process.env.PORT || 3001
+   app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`)
+   })
+}
+
+module.exports = app
+
