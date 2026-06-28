8c121e71917cefd9680cb5e5f3fe677383d8da75
BagasApg <bagasapg@gmail.com>
2026-06-28 21:09:52 +0700

    part3 - added dist/ to backend dir, to prod

--- part2/test/src/services/notes.js
+++ part2/test/src/services/notes.js
@@ -1,16 +1,11 @@
 import axios from 'axios'
-const baseURL = 'http://localhost:3001/api/notes'
+const baseURL = '/api/notes'
 
 const getAll = () => {
    const request = axios.get(baseURL)
 
-   const nonExisting = {
-      id: 1000,
-      content: "yahu",
-      important: true,
-   }
 
-   return request.then(response => response.data.concat(nonExisting))
+   return request.then(response => response.data)
 }
 
 const create = newObject => {
